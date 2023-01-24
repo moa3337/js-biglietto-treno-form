11ffc3431fcab5f7ccceb63f8641a547b7556d2c
moa3337 <moa-x@hotmail.com>
2023-01-24 17:57:26 +0100

    lavoro sulla parte di js

--- js/main.js
+++ js/main.js
@@ -2,31 +2,36 @@
 // con sconti del 20% o 40% in base all'età
 // il prezzo è di 0,21 centesimi al km
 
-// PREZZO €/Km
-console.log('Prezzo biglietto: 0.21€/Km');
+// CREAZIONE EVENTO SUL CLICK "GENERA"
+const btnGenera = document.getElementById('genera');
+btnGenera.addEventListener('click',function(){
+    console.log('ho premuto il bottone');
+    // QUESTO CODICE VIENE ESEGUITO QUANDO SI PREME "GENERA"
+    // RECUPERARE: NOME, COGNOME, ETA' E KM
+    
+    const userName = document.getElementById('name').value;
+    const km = document.getElementById('km');
+    const age = document.getElementById('age');
 
-// Km DA PERCORRERE?
-const km = prompt('Quanti Km devi percorrere?');
+    
+    let price = km * 0.21;
+    // SCONTO UNDER 18
+    if (age < 18) {
+        price = price - (price * .2);
+    }
+    // SCONTO 40% OVER 65
+    else if (age >= 65) {
+        price = price - (price * .4);
+    }
+    
+    // ASSEGNAZIONE POSTO E COD CP
+    let seatNumber = Math.floor(Math.random)() * 100;
+    let randomCod = Math.floor(Math.random)() * 100 + 10;
 
-// ETA' PASSEGERO?
-const age = prompt('Quanti anni hai?');
+    // INFORMAZIONE DA STAMPARE SUL BIGLIETTO
+    // document.getElementById('discount').innerHTML =
+    document.getElementById('seat').innerHTML = seatNumber;
+    document.getElementById('cod').innerHTML = randomCod;
+    document.getElementById('price').innerHTML = "€" + price.toFixed(2);
+});
 
-// CALCOLO PER IL PREZZO
-let price = km * 0.21;
-
-// SCONTO 20% UNDER 18
-if(age < 18){
-    price = price - (price * .2);
-}
-// SCONTO 40% OVER 65
-else if(age >= 65){
-    price = price - (price * .4);
-}
-
-// PREZZo CON 2 DECIMALI
-price = price.toFixed(2);
-
-// PREZZO FINALE IN €
-console.log('Il prezzo del tuo biglietto è di €' + price);
-
-// PREZZO NEL TITOLO DELLA PAGINA
