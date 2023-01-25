75295f1183b33f9156b2d8886389746f4a56770d
moa3337 <moa-x@hotmail.com>
2023-01-25 13:45:18 +0100

    final commit

--- js/main.js
+++ js/main.js
@@ -5,33 +5,38 @@
 // CREAZIONE EVENTO SUL CLICK "GENERA"
 const btnGenera = document.getElementById('genera');
 btnGenera.addEventListener('click',function(){
-    console.log('ho premuto il bottone');
-    // QUESTO CODICE VIENE ESEGUITO QUANDO SI PREME "GENERA"
-    // RECUPERARE: NOME, COGNOME, ETA' E KM
-    
-    const userName = document.getElementById('name').value;
-    const km = document.getElementById('km');
-    const age = document.getElementById('age');
 
-    
-    let price = km * 0.21;
-    // SCONTO UNDER 18
+    // Recupera i valori del nome, dei chilometri e dell'età 
+    let name = document.getElementById("name").value;
+    let km = document.getElementById("km").value;
+    let age = document.getElementById("age").value;
+
+    // PREZZO €/Km
+    console.log('prezzo biglietto: 0.21€/Km');
+
+    // Calcola il prezzo base in base ai chilometri
+    let Price = km * 0.21;
+
+    // Applica lo sconto in base all'etÃ 
     if (age < 18) {
-        price = price - (price * .2);
+        Price = Price - (Price * .2);
+    } else if (age >= 65) {
+        Price = Price - (Price * .4);
     }
-    // SCONTO 40% OVER 65
-    else if (age >= 65) {
-        price = price - (price * .4);
-    }
-    
-    // ASSEGNAZIONE POSTO E COD CP
-    let seatNumber = Math.floor(Math.random)() * 100;
-    let randomCod = Math.floor(Math.random)() * 100 + 10;
 
-    // INFORMAZIONE DA STAMPARE SUL BIGLIETTO
-    // document.getElementById('discount').innerHTML =
-    document.getElementById('seat').innerHTML = seatNumber;
-    document.getElementById('cod').innerHTML = randomCod;
-    document.getElementById('price').innerHTML = "€" + price.toFixed(2);
+    // PREZZO FINALE IN €
+    console.log("Il prezzo del tuo biglietto è di €" + Price);
+
+    // Genera un numero casuale per il posto
+    let seatNumber = Math.floor(Math.random() * 100) + 1;
+
+    // Mostra i valori nella pagina
+    document.getElementById("passengerName").innerHTML = name;
+    document.getElementById("distance").innerHTML = km + " km";
+    document.getElementById("seatNumber").innerHTML = seatNumber;
+    document.getElementById("ticketPrice").innerHTML = "€" + Price.toFixed(2);   
 });
+
+    
+
 
